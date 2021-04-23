75842be98faa050a86cc4bf180a753b7a20084b1
Fabian Kägy <mail@fabian-kaegy.de>
2021-04-23 17:52:37 +0200

    initial plugin setup

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: WordPress Archive Pages
+ * Description: Push Archive pages to the next level
+ * Author: Fabian Kägy
+ * Author URI: fabian-kaegy.com
+ * Version: 0.0.1
+ *
+ * @package wordpress-archive-pages
+ */
+
+namespace wordpress_archive_pages;
+
+define( 'WORDPRESS_ARCHIVE_PAGES_PATH', plugin_dir_path( __FILE__ ) );
+define( 'WORDPRESS_ARCHIVE_PAGES_URL', plugin_dir_url( __FILE__ ) );
